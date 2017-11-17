5a0bac0999abaf5bc34651e6b766846fa0b2a99d
Qamar Waqar <qamogle@gmail.com>
2017-11-17 16:04:46 +0500

    Discard Variable
    How to use Discard Variable

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Discard Variable (write only) //
+            var x = 'a';
+            if (x is int) { Console.WriteLine("x is an Integer"); }
+            // note that you cannot use "var" only but "var _" // interesting
+            if (x is var _) { Console.WriteLine("x is a var\nPress any Key to Quit"); }
+            Console.ReadKey(true);
+        }
+    }
+}
